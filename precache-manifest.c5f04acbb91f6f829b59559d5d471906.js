da19e2ce68ee61e66a9638aaa5b86b7b5fb571f9
Maria <mashalarchenko16@gmail.com>
2020-01-22 18:30:41 +0300

    Add files via upload

---
+++ precache-manifest.c5f04acbb91f6f829b59559d5d471906.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e39bec27663c73562fa6c60dee6c0a32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19cfc385c791727bd24d",
+    "url": "/static/css/main.e4ddd353.chunk.css"
+  },
+  {
+    "revision": "fd88580b745e1a3a8592",
+    "url": "/static/js/2.981b25c6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.981b25c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03583d0fbabee4eece17",
+    "url": "/static/js/3.e629bf91.chunk.js"
+  },
+  {
+    "revision": "19cfc385c791727bd24d",
+    "url": "/static/js/main.8ade4192.chunk.js"
+  },
+  {
+    "revision": "7a1b8221cd6ba7f4a396",
+    "url": "/static/js/runtime-main.78fc86b8.js"
+  }
+]);
